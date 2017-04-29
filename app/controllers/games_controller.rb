4ffab5acd23ed145ec36348fb2f7e4d34ee863ff
Nate Rudnick Aldridge <nr265@cornell.edu>
2017-04-29 17:38:07 +0000

    Made a navbar and other fun things

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,14 @@ class GamesController < ApplicationController
   def show
     @game = Game.find_by(number: params[:number])
   end
+  
+  def search
+    search_term = params[:number]
+    if Game.exists?(number: search_term)
+      redirect_to "/games/#{search_term}"
+    else
+      flash[:alert] = "That game doesn't exist!"
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :games, param: :number
+  get "search" => "games#search"
   resources :players, param: :name
 end
